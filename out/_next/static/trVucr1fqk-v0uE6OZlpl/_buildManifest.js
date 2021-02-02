597d8f0ecfa4ce7e2202e1631fcfb68a77ea9cbe
Tod <ootodoo@gmail.com>
2021-02-02 18:44:09 +0700

    commit

---
+++ out/_next/static/trVucr1fqk-v0uE6OZlpl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-a3046adedf66ea6c4908.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1f29d3aa4c65cbb06a55.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
